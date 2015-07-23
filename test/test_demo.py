d4668ea9e48fc18eac3c4fb2095c27a4ab18f059
Andriy Melnyk <andriy.melnyk@gmail.com>
2015-07-23 16:05:13 +0300

    Phase 0

---
+++ test/test_demo.py
@@ -0,0 +1,10 @@
+import unittest
+from app import demo
+
+class TestDemo(unittest.TestCase):
+	def test_demo(self):
+		self.test_app = demo.app.test_client()
+		response = self.test_app.get('/')
+		self.assertEquals(response.status, '200 OK')
+		self.assertEquals(response.headers['Content-Type'], "text/html; charset=utf-8")
+		self.assertEquals(response.data, 'Hello World!')
