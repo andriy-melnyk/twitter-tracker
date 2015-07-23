49e7e0d3d9a5032782f3aaeb0b269ab84d33ce8e
Andriy Melnyk <andriy.melnyk@gmail.com>
2015-07-23 16:24:03 +0300

    Phase 0: Freeze packages

--- install_venv.sh
+++ install_venv.sh
@@ -2,4 +2,4 @@ sudo apt-get install python-virtualenv
 sudo apt-get install python-pip
 virtualenv venv
 source venv/bin/activate
-pip install --upgrade Flask Flask-RESTful mock nose requests SQLAlchemy
+pip install -r requirements.txt
